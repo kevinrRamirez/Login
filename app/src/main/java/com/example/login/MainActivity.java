55c114cddb4bc0a7b1b46a3c1263e040866a01df
kevinrRamirez <dramirezv@toluca.tecnm.mx>
2019-11-05 21:26:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ctrlBoton(View view)
+    {
+        Intent intent = null;
+
+        switch (view.getId())
+        {
+            case R.id.btnIngresar:
+                intent = new Intent(MainActivity.this, NavigationPaseando.class);
+                break;
+            case R.id.btnRegistro:
+                intent = new Intent(MainActivity.this, Registro.class);
+                break;
+
+        }
+        startActivity(intent);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/login/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.login.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
