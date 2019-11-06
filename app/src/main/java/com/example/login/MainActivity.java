b888be62be57ec8a3440ca447a994cf1055faa9e
orlando1998gonzalez <54719769+orlando1998gonzalez@users.noreply.github.com>
2019-11-05 22:28:58 -0600

    prb1

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+    ahorita vemos que pedo
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
