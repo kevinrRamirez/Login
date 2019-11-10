330611480946ee480d98ecda1a9515347c1764c9
orlando1998gonzalez <54719769+orlando1998gonzalez@users.noreply.github.com>
2019-11-10 16:46:50 -0600

    validacion campos del registro

--- app/src/main/java/com/example/login/Registro.java
+++ app/src/main/java/com/example/login/Registro.java
@@ -7,14 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Registro extends AppCompatActivity  {
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Registro extends AppCompatActivity {
 
     EditText txtNombre;
     EditText txtCorreo;
     EditText txtPass;
-    EditText txtPassConf ;
-    Button btnIngresar ;
+    EditText txtPassConf;
+    Button btnIngresar;
+
+    TextView tvAux;
 
 
     @Override
@@ -22,11 +30,14 @@ public class Registro extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        txtNombre = (EditText)findViewById(R.id.txtNombre);
-        txtCorreo = (EditText)findViewById(R.id.txtCorreo);
-        txtPass = (EditText)findViewById(R.id.txtPass);
-        txtPassConf = (EditText)findViewById(R.id.txtPassConf);
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        txtCorreo = (EditText) findViewById(R.id.txtCorreo);
+        txtPass = (EditText) findViewById(R.id.txtPass);
+        txtPassConf = (EditText) findViewById(R.id.txtPassConf);
         btnIngresar = (Button) findViewById(R.id.btnIngresar);
+
+        tvAux = (TextView) findViewById(R.id.tv_aux);
+
         //btnIngresar.setOnClickListener(this);
     }
 
@@ -50,20 +61,91 @@ public class Registro extends AppCompatActivity  {
     }
 
     public void ctrlBoton(View view) {
-
         Intent intent = null;
+        switch (view.getId()) {
+            /*
+             case R.id.btnRegistro:
 
-        switch (view.getId())
-        {
-            case R.id.btnRegistro:
                 intent = new Intent(Registro.this, NavigationPaseando.class);
                 break;
+
+             */
+
             case R.id.tv_ingresar:
                 intent = new Intent(Registro.this, MainActivity.class);
                 break;
-
         }
         startActivity(intent);
+    }
+
+
+    public void Registrar(View view) {
+        String nombre = txtNombre.getText().toString();
+        String correo = txtCorreo.getText().toString();
+        String pass = txtPass.getText().toString();
+        String passConf = txtPassConf.getText().toString();
+
+        if (nombre.isEmpty() || correo.isEmpty() || pass.isEmpty() || passConf.isEmpty()) {
+            Toast.makeText(this, "Todos los campos son requeridos", Toast.LENGTH_LONG).show();
+        } else if (validacionNombreApellido(nombre) && validacionCorreo(correo)) {
+            if (!(pass.length() >= 6)) {
+                Toast.makeText(this, "Se requiere una contraseña mayor a 5 caracteres", Toast.LENGTH_LONG).show();
+            } else if (!pass.equals(passConf)) {
+                Toast.makeText(this, "Las contaseñas no coinciden", Toast.LENGTH_LONG).show();
+            }else{
+                Intent intent = new Intent(view.getContext(), NavigationPaseando.class);
+                startActivity(intent);
+            }
+        }
 
     }
+
+
+    boolean validacionNombreApellido(String s) {
+        int cont = 0;
+        boolean bandera = true;
+        StringTokenizer t = new StringTokenizer(s, " ");
+        if (t.countTokens() != 2) {
+            Toast.makeText(this, "Ingresar Nombre y Apellido", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        while (t.hasMoreTokens()) {
+            if (!validacionNombre(t.nextToken())) {
+                Toast.makeText(this, "Ingresar Nombre y Apellido de forma correcta", Toast.LENGTH_LONG).show();
+                bandera = false;
+            }
+            cont++;
+        }
+        return bandera;
+    }
+
+    boolean validacionNombre(String s) {
+        boolean bandera = true;
+        String ABC = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+        String abc = "abcdefghijklmnñopqrstuvwxyz";
+        if (!ABC.contains(Character.toString(s.charAt(0)))) {
+            return false;
+        }
+        for (int i = 1; i < s.length(); i++) {
+            if (!abc.contains(Character.toString(s.charAt(i)))) {
+                return false;
+            }
+        }
+        return bandera;
+    }
+
+    boolean validacionCorreo(String s) {
+        // Patrón para validar el email
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher mather = pattern.matcher(s);
+        if (mather.find()) {
+            return true;
+        } else {
+            Toast.makeText(this, "Correo electronico invalido", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+    }
+
 }
