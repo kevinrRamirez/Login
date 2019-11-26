26cedacaa3113f2275c3c5abd122e510e7c42b57
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2019-11-26 00:29:38 -0600

    registro Perro, ahi va

--- app/src/main/java/com/example/login/RegistroPerro.java
+++ app/src/main/java/com/example/login/RegistroPerro.java
@@ -2,7 +2,9 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistroPerro extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class RegistroPerro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_perro);
     }
+
+
+    public void ctrlBtnRegistroPerro(View v) {
+        Intent intent = new Intent(v.getContext(), NavigationPaseando.class);
+        startActivity(intent);
+    }
+
 }
