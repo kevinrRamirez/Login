7ef0d3aaabb2d4394c5e66102ad988ff0789ae26
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2019-11-25 23:00:07 -0600

    nueva imagen en construccion y cambio en fragment_home

--- app/src/main/java/com/example/login/NavigationPaseando.java
+++ app/src/main/java/com/example/login/NavigationPaseando.java
@@ -76,7 +76,11 @@ public class NavigationPaseando extends AppCompatActivity {
         Intent intent = new Intent(view.getContext(),PedirPaseo.class);
         startActivity(intent);
     }
-
+    public void ctrlBotonHospedaje(View view)
+    {
+        Intent intent = new Intent(view.getContext(),Hospedaje.class);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
